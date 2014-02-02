2f39532b20567271c91b96a64b72a3a647a6a5cc
Fabien TODESCATO <fabien.todescato@gmail.com>
2014-02-02 19:30:34 +0100

    Clean up clojure tests.

--- src/test/clojure/net/ftod/zcube_test.clj
+++ src/test/clojure/net/ftod/zcube_test.clj
@@ -4,17 +4,15 @@
 
 ( deftest test-sum-1
   ( is
-    ( let [ zn ( sum-subtrees [ [ 5 ( path "a" "b" "c" ) ]
-                              , [ 10 ( path "a" "b" "d" ) ]
-                              ] ) ]
-      ( = ( map #( ( count-trees % ) zn )
-            [ ( path "a" )
-            , ( path "a" "b" )
-            , ( path "a" "b" "c" )
-            , ( path "a" "b" "d" )
-            ]
-          )
-          [ 15 15 5 10 ]
+    ( let [ zn ( sum-subtrees
+                 [ [  5 ( path "a" "b" "c" ) ]
+                   [ 10 ( path "a" "b" "d" ) ]
+                 ] ) ]
+      ( and
+        ( = 15 ( ( count-trees ( path "a" )         ) zn ) )
+        ( = 15 ( ( count-trees ( path "a" "b" )     ) zn ) )
+        ( =  5 ( ( count-trees ( path "a" "b" "c" ) ) zn ) )
+        ( = 10 ( ( count-trees ( path "a" "b" "d" ) ) zn ) )
       )
     )
   )
@@ -22,11 +20,11 @@
 
 ( deftest test-sum-2
   ( is ( let [ zn ( sum-subtrees
-                      [ [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "male"   ) ( path "2014" "01" "01" "10" "32" ) ) ]
-                      , [ 1 ( product ( path "www.company.com" "page2" ) ( path "gender" "female" ) ( path "2014" "01" "02" "11" "35" ) ) ]
-                      , [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "female" ) ( path "2014" "01" "03" "08" "15" ) ) ]
-                      ]
-                    )
+                    [ [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "male"   ) ( path "2014" "01" "01" "10" "32" ) ) ]
+                    , [ 1 ( product ( path "www.company.com" "page2" ) ( path "gender" "female" ) ( path "2014" "01" "02" "11" "35" ) ) ]
+                    , [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "female" ) ( path "2014" "01" "03" "08" "15" ) ) ]
+                    ]
+                  )
              ]
       ( = ( map #( ( count-trees % ) zn )
             [ ( path "www.company.com" )
