036b9d22ba948cd6c42ad38a462531b70b63fdc2
Fabien TODESCATO <fabien.todescato@gmail.com>
2014-02-02 19:23:21 +0100

    Clean up clojure test.

--- src/test/clojure/net/ftod/zcube_test.clj
+++ src/test/clojure/net/ftod/zcube_test.clj
@@ -2,25 +2,33 @@
   ( :use net.ftod.zcube clojure.test )
 )
 
-( deftest test-cube-1
+( deftest test-sum-1
   ( is
-    ( let [ cube ( sum-subtrees [ [ 5 ( path "a" "b" "c" ) ] [ 10 ( path "a" "b" "d" ) ] ] ) ]
-      ( = ( map #( ( count-trees % ) cube ) [ ( path "a" ) ( path "a" "b" ) ( path "a" "b" "c" ) ( path "a" "b" "d" ) ] )
+    ( let [ zn ( sum-subtrees [ [ 5 ( path "a" "b" "c" ) ]
+                              , [ 10 ( path "a" "b" "d" ) ]
+                              ] ) ]
+      ( = ( map #( ( count-trees % ) zn )
+            [ ( path "a" )
+            , ( path "a" "b" )
+            , ( path "a" "b" "c" )
+            , ( path "a" "b" "d" )
+            ]
+          )
           [ 15 15 5 10 ]
       )
     )
   )
 )
 
-( deftest test-cube-2
-  ( is ( let [ cube ( sum-subtrees
+( deftest test-sum-2
+  ( is ( let [ zn ( sum-subtrees
                       [ [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "male"   ) ( path "2014" "01" "01" "10" "32" ) ) ]
                       , [ 1 ( product ( path "www.company.com" "page2" ) ( path "gender" "female" ) ( path "2014" "01" "02" "11" "35" ) ) ]
                       , [ 1 ( product ( path "www.company.com" "page1" ) ( path "gender" "female" ) ( path "2014" "01" "03" "08" "15" ) ) ]
                       ]
                     )
              ]
-      ( = ( map #( ( count-trees % ) cube )
+      ( = ( map #( ( count-trees % ) zn )
             [ ( path "www.company.com" )
             , ( path "www.company.com" "page1" )
             , ( path "2014" "01" )
@@ -33,29 +41,3 @@
           , 2
           ]
       ) ) ) )
-
-;( def big-cube
-;  ( make-cube
-;    ( for
-;      [ year [ 2011 2012 2013 ]
-;      , month ( range 12 )
-;      , day ( range 31 ) ; FIXME Function of month...
-;      , hour (  range 24 )
-;      , minute ( range 60 )
-;      , second ( range 60 )
-;      ]
-;      [ 1 ( apply path ( map str [
-;                                    year
-;                                    month
-;                                    day
-;;                                    hour
-;;                                    minute
-;;                                    second
-;                                    ] ) ) ]
-;    )
-;  )
-;)
-
-; ( measure big-cube ( trees bot ) )
-; ( measure big-cube ( trees top ) )
-; ( measure big-cube ( trees ( path "2011" ) ) )
