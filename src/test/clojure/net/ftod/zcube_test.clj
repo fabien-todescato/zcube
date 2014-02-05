07eafe17f6867b67a5390d131044ffc134c12985
Fabien TODESCATO <fabien.todescato@gmail.com>
2014-02-05 09:52:47 +0100

    Format analytics example.

--- src/test/clojure/net/ftod/zcube_test.clj
+++ src/test/clojure/net/ftod/zcube_test.clj
@@ -45,28 +45,28 @@
         ( =  3 ( ( count-trees ( product ( path "a" "b" ) ( path "a" "d" ) ) ) zn ) )
       ) ) ) )
 
-( deftest test-sum-4 ; Analytics example
+( deftest test-analytics ; Analytics example
   ( is
     ( let [ zn ( sum-subtrees
                  [ [ 1 ( product
-                      ( path "www.company.com" "page1" )
-                      ( path "gender" "male" )
-                      ( path "2014" "01" "01" "10" "32" ) ) ]
-                    , [ 1 ( product
-                      ( path "www.company.com" "page2" )
-                      ( path "gender" "female" )
-                      ( path "2014" "01" "02" "11" "35" ) ) ]
-                    , [ 1 ( product
-                      ( path "www.company.com" "page1" )
-                      ( path "gender" "female" )
-                      ( path "2014" "01" "03" "08" "15" ) ) ]
-                    ]
+                         ( path "www.company.com" "page1" )
+                         ( path "gender" "male" )
+                         ( path "2014" "01" "01" "10" "32" ) ) ]
+                 , [ 1 ( product
+                         ( path "www.company.com" "page2" )
+                         ( path "gender" "female" )
+                         ( path "2014" "01" "02" "11" "35" ) ) ]
+                 , [ 1 ( product
+                         ( path "www.company.com" "page1" )
+                         ( path "gender" "female" )
+                         ( path "2014" "01" "03" "08" "15" ) ) ]
+                 ]
                ) ]
       ( and
-        ( = 3 ( ( count-trees ( path "www.company.com" )         ) zn ) )
-        ( = 2 ( ( count-trees ( path "www.company.com" "page1" ) ) zn ) )
-        ( = 3 ( ( count-trees ( path "2014" "01" )               ) zn ) )
-        ( = 2 ( ( count-trees ( path "gender" "female" )         ) zn ) )
-        ( = 2 ( ( count-trees ( product ( path "gender" "female" ) ( path "2014" "01" ) ) ) zn ) )
+        ( = 3 ( ( count-trees ( path "www.company.com" )                                       ) zn ) )
+        ( = 2 ( ( count-trees ( path "www.company.com" "page1" )                               ) zn ) )
+        ( = 3 ( ( count-trees ( path "2014" "01" )                                             ) zn ) )
+        ( = 2 ( ( count-trees ( path "gender" "female" )                                       ) zn ) )
+        ( = 2 ( ( count-trees ( product ( path "gender" "female" ) ( path "2014" "01" ) )      ) zn ) )
         ( = 1 ( ( count-trees ( product ( path "gender" "female" ) ( path "2014" "01" "02" ) ) ) zn ) )
       ) ) ) )
