cbb69037cbb3e6ab5db77a432fb164df1b82531b
Fabien TODESCATO <fabien.todescato@gmail.com>
2014-02-07 21:47:43 +0100

    Fix typos. Qualify zcube namespace. Add nutshell section.

--- src/test/clojure/net/ftod/zcube_test.clj
+++ src/test/clojure/net/ftod/zcube_test.clj
@@ -1,72 +1,73 @@
 (ns net.ftod.zcube-test
-  ( :use net.ftod.zcube clojure.test )
+  ( :use clojure.test )
+  ( :require [ net.ftod.zcube :as z ] )
 )
 
 ( deftest test-sum-1 ; Linear trees example
   ( is
-    ( let [ zn ( sum-subtrees
-               [ [  5 ( path "a" "b" "c" ) ]
-                 [ 10 ( path "a" "b" "d" ) ]
+    ( let [ zn ( z/sum-subtrees
+               [ [  5 ( z/path "a" "b" "c" ) ]
+                 [ 10 ( z/path "a" "b" "d" ) ]
                ] ) ]
     ( and
-      ( = 15 ( ( count-trees ( path "a" )         ) zn ) )
-      ( = 15 ( ( count-trees ( path "a" "b" )     ) zn ) )
-      ( =  5 ( ( count-trees ( path "a" "b" "c" ) ) zn ) )
-      ( = 10 ( ( count-trees ( path "a" "b" "d" ) ) zn ) )
+      ( = 15 ( ( z/count-trees ( z/path "a" )         ) zn ) )
+      ( = 15 ( ( z/count-trees ( z/path "a" "b" )     ) zn ) )
+      ( =  5 ( ( z/count-trees ( z/path "a" "b" "c" ) ) zn ) )
+      ( = 10 ( ( z/count-trees ( z/path "a" "b" "d" ) ) zn ) )
     ) ) ) )
 
 ( deftest test-sum-2 ; Branching trees example
   ( is
-    ( let [ zn ( sum-subtrees
-               [ [ 1 ( cross ( path "a" "b" ) ( path "a" "c" ) ) ] 
-               , [ 1 ( cross ( path "a" "b" ) ( path "a" "d" ) ) ]
+    ( let [ zn ( z/sum-subtrees
+               [ [ 1 ( z/cross ( z/path "a" "b" ) ( z/path "a" "c" ) ) ] 
+               , [ 1 ( z/cross ( z/path "a" "b" ) ( z/path "a" "d" ) ) ]
                ] ) ]
       ( and
-        ( =  2 ( ( count-trees ( path "a" ) ) zn ) )
-        ( =  2 ( ( count-trees ( path "a" "b" ) ) zn ) )
-        ( =  1 ( ( count-trees ( path "a" "c" ) ) zn ) )
-        ( =  1 ( ( count-trees ( cross ( path "a" "b" ) ( path "a" "c" ) ) ) zn ) )
-        ( =  1 ( ( count-trees ( path "a" "d" ) ) zn ) )
-        ( =  1 ( ( count-trees ( cross ( path "a" "b" ) ( path "a" "d" ) ) ) zn ) )
+        ( =  2 ( ( z/count-trees ( z/path "a" ) ) zn ) )
+        ( =  2 ( ( z/count-trees ( z/path "a" "b" ) ) zn ) )
+        ( =  1 ( ( z/count-trees ( z/path "a" "c" ) ) zn ) )
+        ( =  1 ( ( z/count-trees ( z/cross ( z/path "a" "b" ) ( z/path "a" "c" ) ) ) zn ) )
+        ( =  1 ( ( z/count-trees ( z/path "a" "d" ) ) zn ) )
+        ( =  1 ( ( z/count-trees ( z/cross ( z/path "a" "b" ) ( z/path "a" "d" ) ) ) zn ) )
       ) ) ) )
 
 ( deftest test-sum-3 ; Branching trees example
   ( is
-    ( let [ zn ( sum-subtrees
-               [ [ 5 ( cross ( path "a" "b" ) ( path "a" "c" ) ) ] 
-               , [ 3 ( cross ( path "a" "b" ) ( path "a" "d" ) ) ]
+    ( let [ zn ( z/sum-subtrees
+               [ [ 5 ( z/cross ( z/path "a" "b" ) ( z/path "a" "c" ) ) ] 
+               , [ 3 ( z/cross ( z/path "a" "b" ) ( z/path "a" "d" ) ) ]
                ] ) ]
       ( and
-        ( =  8 ( ( count-trees ( path "a" ) ) zn ) )
-        ( =  8 ( ( count-trees ( path "a" "b" ) ) zn ) )
-        ( =  5 ( ( count-trees ( path "a" "c" ) ) zn ) )
-        ( =  5 ( ( count-trees ( cross ( path "a" "b" ) ( path "a" "c" ) ) ) zn ) )
-        ( =  3 ( ( count-trees ( path "a" "d" ) ) zn ) )
-        ( =  3 ( ( count-trees ( cross ( path "a" "b" ) ( path "a" "d" ) ) ) zn ) )
+        ( =  8 ( ( z/count-trees ( z/path "a" )                                    ) zn ) )
+        ( =  8 ( ( z/count-trees ( z/path "a" "b" )                                ) zn ) )
+        ( =  5 ( ( z/count-trees ( z/path "a" "c" )                                ) zn ) )
+        ( =  5 ( ( z/count-trees ( z/cross ( z/path "a" "b" ) ( z/path "a" "c" ) ) ) zn ) )
+        ( =  3 ( ( z/count-trees ( z/path "a" "d" )                                ) zn ) )
+        ( =  3 ( ( z/count-trees ( z/cross ( z/path "a" "b" ) ( z/path "a" "d" ) ) ) zn ) )
       ) ) ) )
 
 ( deftest test-analytics ; Analytics example
   ( is
-    ( let [ zn ( sum-subtrees
-                 [ [ 1 ( cross
-                         ( path "www.company.com" "page1" )
-                         ( path "gender" "male" )
-                         ( path "2014" "01" "01" "10" "32" ) ) ]
-                 , [ 1 ( cross
-                         ( path "www.company.com" "page2" )
-                         ( path "gender" "female" )
-                         ( path "2014" "01" "02" "11" "35" ) ) ]
-                 , [ 1 ( cross
-                         ( path "www.company.com" "page1" )
-                         ( path "gender" "female" )
-                         ( path "2014" "01" "03" "08" "15" ) ) ]
+    ( let [ zn ( z/sum-subtrees
+                 [ [ 1 ( z/cross
+                         ( z/path "www.company.com" "page1" )
+                         ( z/path "gender" "male" )
+                         ( z/path "2014" "01" "01" "10" "32" ) ) ]
+                 , [ 1 ( z/cross
+                         ( z/path "www.company.com" "page2" )
+                         ( z/path "gender" "female" )
+                         ( z/path "2014" "01" "02" "11" "35" ) ) ]
+                 , [ 1 ( z/cross
+                         ( z/path "www.company.com" "page1" )
+                         ( z/path "gender" "female" )
+                         ( z/path "2014" "01" "03" "08" "15" ) ) ]
                  ]
                ) ]
       ( and
-        ( = 3 ( ( count-trees ( path "www.company.com" )                                       ) zn ) )
-        ( = 2 ( ( count-trees ( path "www.company.com" "page1" )                               ) zn ) )
-        ( = 3 ( ( count-trees ( path "2014" "01" )                                             ) zn ) )
-        ( = 2 ( ( count-trees ( path "gender" "female" )                                       ) zn ) )
-        ( = 2 ( ( count-trees ( cross ( path "gender" "female" ) ( path "2014" "01" ) )      ) zn ) )
-        ( = 1 ( ( count-trees ( cross ( path "gender" "female" ) ( path "2014" "01" "02" ) ) ) zn ) )
+        ( = 3 ( ( z/count-trees ( z/path "www.company.com" )                                         ) zn ) )
+        ( = 2 ( ( z/count-trees ( z/path "www.company.com" "page1" )                                 ) zn ) )
+        ( = 3 ( ( z/count-trees ( z/path "2014" "01" )                                               ) zn ) )
+        ( = 2 ( ( z/count-trees ( z/path "gender" "female" )                                         ) zn ) )
+        ( = 2 ( ( z/count-trees ( z/cross ( z/path "gender" "female" ) ( z/path "2014" "01" ) )      ) zn ) )
+        ( = 1 ( ( z/count-trees ( z/cross ( z/path "gender" "female" ) ( z/path "2014" "01" "02" ) ) ) zn ) )
       ) ) ) )
