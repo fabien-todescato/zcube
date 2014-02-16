a5370e71f14ed41e02e446b5723679954ceb26e4
Fabien TODESCATO <fabien.todescato@gmail.com>
2014-02-16 09:14:11 +0100

    Add bulk write operation to ZDDLong.

--- src/main/java/net/ftod/zcube/zdd/ZDDLong.java
+++ src/main/java/net/ftod/zcube/zdd/ZDDLong.java
@@ -1,7 +1,10 @@
 package net.ftod.zcube.zdd;
 
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -25,6 +28,29 @@ public final class ZDDLong {
     {
         dos.writeLong(l);
         t.write(dos);
+    }
+
+    public static void write(final Iterable<ZDDLong> i, final File file) throws IOException
+    {
+        final DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+
+        try {
+            write(i, dos);
+        } finally {
+            dos.close();
+        }
+    }
+
+    public static void write(final Iterable<ZDDLong> i, final DataOutputStream dos) throws IOException
+    {
+        write(i.iterator(), dos);
+    }
+
+    public static void write(final Iterator<ZDDLong> i, final DataOutputStream dos) throws IOException
+    {
+        while (i.hasNext()) {
+            i.next().write(dos);
+        }
     }
 
     public static ZDDLong read(final DataInputStream dis) throws IOException
